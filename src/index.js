544109d87f6030dfeab773a1d4f719a06a1bd8f4
zlocate <43764701+zlocate@users.noreply.github.com>
2023-12-10 15:11:13 +0300

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -1,152 +1,112 @@
-// Website you intended to retrieve for users.
-const upstream = 'www.google.com'
-
-// Custom pathname for the upstream website.
-const upstream_path = '/'
-
-// Website you intended to retrieve for users using mobile devices.
-const upstream_mobile = 'www.google.com'
-
-// Countries and regions where you wish to suspend your service.
-const blocked_region = ['CN', 'KP', 'SY', 'PK', 'CU']
-
-// IP addresses which you wish to block from using your service.
-const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
-
-// Whether to use HTTPS protocol for upstream address.
-const https = true
-
-// Whether to disable cache.
-const disable_cache = true
-
-// Replace texts.
-const replace_dict = {
-    '$upstream': '$custom_domain',
-    '//google.com': ''
-}
-
-addEventListener('fetch', event => {
-    event.respondWith(fetchAndApply(event.request));
-})
-
-async function fetchAndApply(request) {
-
-    const region = request.headers.get('cf-ipcountry').toUpperCase();
-    const ip_address = request.headers.get('cf-connecting-ip');
-    const user_agent = request.headers.get('user-agent');
-
-    let response = null;
-    let url = new URL(request.url);
-    let url_hostname = url.hostname;
-
-    if (https == true) {
-        url.protocol = 'https:';
-    } else {
-        url.protocol = 'http:';
-    }
-
-    if (await device_status(user_agent)) {
-        var upstream_domain = upstream;
-    } else {
-        var upstream_domain = upstream_mobile;
-    }
-
-    url.host = upstream_domain;
-    if (url.pathname == '/') {
-        url.pathname = upstream_path;
-    } else {
-        url.pathname = upstream_path + url.pathname;
-    }
-
-    if (blocked_region.includes(region)) {
-        response = new Response('Access denied: WorkersProxy is not available in your region yet.', {
-            status: 403
-        });
-    } else if (blocked_ip_address.includes(ip_address)) {
-        response = new Response('Access denied: Your IP address is blocked by WorkersProxy.', {
-            status: 403
-        });
-    } else {
-        let method = request.method;
-        let request_headers = request.headers;
-        let new_request_headers = new Headers(request_headers);
-
-        new_request_headers.set('Host', upstream_domain);
-        new_request_headers.set('Referer', url.protocol + '//' + url_hostname);
-
-        let original_response = await fetch(url.href, {
-            method: method,
-            headers: new_request_headers
-        })
-
-        let original_response_clone = original_response.clone();
-        let original_text = null;
-        let response_headers = original_response.headers;
-        let new_response_headers = new Headers(response_headers);
-        let status = original_response.status;
-		
-		if (disable_cache) {
-			new_response_headers.set('Cache-Control', 'no-store');
-	    }
-
-        new_response_headers.set('access-control-allow-origin', '*');
-        new_response_headers.set('access-control-allow-credentials', true);
-        new_response_headers.delete('content-security-policy');
-        new_response_headers.delete('content-security-policy-report-only');
-        new_response_headers.delete('clear-site-data');
-		
-        if(new_response_headers.get("x-pjax-url")) {
-            new_response_headers.set("x-pjax-url", response_headers.get("x-pjax-url").replace("//" + upstream_domain, "//" + url_hostname));
+addEventListener("fetch", (event) => {
+    event.respondWith(
+      handleRequest(event.request).catch(
+        (err) => new Response(err.stack, { status: 500 })
+      )
+    );
+  });
+  
+  async function handleRequest(request) {
+    const url = getUrl(request)
+    console.log(`${request.method} ${url}`)
+    if (!url) {
+      return new Response(
+        Page,
+        {
+          headers: { "Content-Type": "text/html" },
+          status: 404,
         }
-		
-        const content_type = new_response_headers.get('content-type');
-        if (content_type != null && content_type.includes('text/html') && content_type.includes('UTF-8')) {
-            original_text = await replace_response_text(original_response_clone, upstream_domain, url_hostname);
-        } else {
-            original_text = original_response_clone.body
-        }
-		
-        response = new Response(original_text, {
-            status,
-            headers: new_response_headers
-        })
+      )
     }
-    return response;
-}
-
-async function replace_response_text(response, upstream_domain, host_name) {
-    let text = await response.text()
-
-    var i, j;
-    for (i in replace_dict) {
-        j = replace_dict[i]
-        if (i == '$upstream') {
-            i = upstream_domain
-        } else if (i == '$custom_domain') {
-            i = host_name
-        }
-
-        if (j == '$upstream') {
-            j = upstream_domain
-        } else if (j == '$custom_domain') {
-            j = host_name
-        }
-
-        let re = new RegExp(i, 'g')
-        text = text.replace(re, j);
-    }
-    return text;
-}
-
-
-async function device_status(user_agent_info) {
-    var agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
-    var flag = true;
-    for (var v = 0; v < agents.length; v++) {
-        if (user_agent_info.indexOf(agents[v]) > 0) {
-            flag = false;
-            break;
-        }
-    }
-    return flag;
-}
+    let response = await fetch(new Request(url, {
+      body: request.body,
+      headers: request.headers,
+      method: request.method,
+      redirect: "follow",
+    }))
+    let { readable, writable } = new TransformStream()
+    response.body.pipeTo(writable)
+    return new Response(
+      readable, 
+      {
+        headers: response.headers,
+        status: response.status,
+      }
+    )
+  }
+  
+  const getUrl = (request) => {
+      const { pathname, searchParams } = new URL(request.url)
+      if (pathname.startsWith("/http")) {
+          return pathname.slice(1).replace(/http:\/(?!\/)/, "http://",).replace(/https:\/(?!\/)/, "https://")
+      }
+      const searchParamsUrl = searchParams.get("url");
+      if (searchParamsUrl?.startsWith("http")) {
+          return searchParamsUrl
+      }
+  }
+  
+  const Page = `<!DOCTYPE html>
+  <html>
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Proxy | Created by aber</title>
+  </head>
+  
+  <body>
+      <form>
+          <div>
+              <input name="url" type="url" />
+              <button type="submit">→</button>
+          </div>
+      </form>
+      <style>
+          body {
+              background-color: #fafafa;
+          }
+  
+          form {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+              margin-top: 100px;
+          }
+  
+          div {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: center;
+              border: solid 1px #fafafa;
+              box-shadow: 0 0 10px #eee;
+          }
+  
+          input {
+              width: calc(240px + 7vw);
+              height: 40px;
+              border-radius: 0px;
+              border: none;
+              outline: none;
+              padding: 0 10px;
+              flex: 1;
+          }
+  
+          button {
+              width: 50px;
+              height: 40px;
+              border-radius: 0px;
+              border: none;
+              background-color: #fff;
+              color: #000;
+              font-size: 16px;
+              cursor: pointer;
+          }
+      </style>
+  </body>
+  
+  </html>
+  `
